9f377a28e8814cc8cc453a35bf200dbed4c44d1e
Dea-k <lee5250@fredonia.edu>
2018-07-03 00:01:02 -0400

    Data modeling - User, DailyCalory, Diet, Meal, Consumable, Join

--- app/src/main/java/com/example/deakyu/fitnessapp/persistence/dao/ConsumableMealJoinDao.kt
+++ app/src/main/java/com/example/deakyu/fitnessapp/persistence/dao/ConsumableMealJoinDao.kt
@@ -3,10 +3,12 @@ package com.example.deakyu.fitnessapp.persistence.dao
 import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import com.example.deakyu.fitnessapp.calorie.diet.model.MealLocal
 import com.example.deakyu.fitnessapp.persistence.model.ConsumableLocal
 import com.example.deakyu.fitnessapp.persistence.model.ConsumableMealJoin
+import com.example.deakyu.fitnessapp.persistence.model.DailyCalory
 
 @Dao
 interface ConsumableMealJoinDao {
