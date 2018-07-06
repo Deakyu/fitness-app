a666dbd930f6008a8cd1e19b8bc824169a3175ba
Armando Flores <jesus.armando8@gmail.com>
2018-07-06 15:41:50 -0400

    connected cardviewn and layout of jesus

--- app/src/main/java/com/example/deakyu/fitnessapp/AddFoodFragment.kt
+++ app/src/main/java/com/example/deakyu/fitnessapp/AddFoodFragment.kt
@@ -13,7 +13,7 @@ class AddFoodFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_add_food, container, false)
+        return inflater.inflate(R.layout.fragment_consumable_setup, container, false)
     }
 
 }
